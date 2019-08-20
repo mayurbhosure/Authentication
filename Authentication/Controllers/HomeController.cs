0ce070da60c6fd16eb3353e54290c56ad620c9ee
mayurbhosure <45959986+mayurbhosure@users.noreply.github.com>
2019-08-20 17:14:08 +0530

    Initial commit

---
+++ Authentication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace Authentication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Editor")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
